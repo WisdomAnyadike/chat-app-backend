1b0023c781d5e8fe66ebb0a36ea0c52a7c70536a
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-07-04 15:51:09 +0100

    fixed bug

--- Routes/roomRoutes.js
+++ Routes/roomRoutes.js
@@ -3,7 +3,7 @@ const { getRooms, getRoomId } = require('../controller/roomcontroller')
 const router2 = express.Router()
 
 router2.get('/:room_id', getRooms)
-router2.get('/getid', getRoomId)
+router2.post('/getid', getRoomId)
 
 
 
