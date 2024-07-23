d46e8aad9a089f549e77a01aafc317e248dbde3a
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-07-23 14:39:22 +0100

    solve bug

--- models/applyModel.js
+++ models/applyModel.js
@@ -18,7 +18,7 @@ const applySchema = new mongoose.Schema({
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'UserSchema',
         required: true
     },
     profileId: {
